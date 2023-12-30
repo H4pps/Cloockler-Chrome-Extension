ef65cbaf8ccae8af5efe0710399d90b37f6f4975
H4pps <150532087+H4pps@users.noreply.github.com>
2023-12-30 01:04:13 +0100

    added scrolling list with insert and delete functions

--- popup/popup.js
+++ popup/popup.js
@@ -2,19 +2,31 @@ const inputField = document.getElementById("url-in");
 const addButton = document.getElementById("btn-add");
 const localStorageSavedID = "savedSites";
 let sites = []; // can be done with a map in the future #CHANGE
+
 getSitesListFromMemory();
 console.log(sites)
+renderList();
 
-addButton.addEventListener("click", function() {
+addButton.addEventListener("click", inputEvent);
+inputField.addEventListener("keypress", function(event){
+    if (event.key === "Enter") {
+        event.preventDefault();
+        inputEvent();
+    }
+});
+
+function inputEvent() {
     const site = inputField.value; // getting the string from intput field
     
     if (isCorrectURL(site) && !sites.includes(site)) {
         sites.push(site);
+        addElementToDisplay(document.getElementById("url-list"), site);
         saveSitesListToMemory(); // can be changed later to save to only on closing the window #CHANGE
     }
 
+    inputField.value = "";
     console.log(sites);
-});
+}
 
 function isCorrectURL(URL) { // change later to connection check #CHANGE
     if (URL == "") {
@@ -32,6 +44,63 @@ function getSitesListFromMemory() { // the whole function can be more efficient,
     }
 }
 
-function saveSitesListToMemory() {
+function saveSitesListToMemory() { // saving sites to the local storage memory
     localStorage.setItem(localStorageSavedID, JSON.stringify(sites));
+}
+
+function renderList() {
+    const urlList = document.getElementById("url-list");
+    for (let i = 0; i < sites.length; ++i) {
+        addElementToDisplay(urlList, sites[i]);
+    }
+}
+
+function addElementToDisplay(urlList, URL) { // adding an HTML object representing the new element (with given URL)
+    const newItem = document.createElement("div"); // wrap for the URL and delete button
+    const ID = Math.random(); // setting a random id to the HTML object
+    newItem.id = "item-" + ID;
+    newItem.className = 'list-item';
+    
+    const itemUrl = document.createElement("span"); // displaying URL
+    itemUrl.textContent = URL;
+    const deleteButton = document.createElement("button"); // delete button
+    deleteButton.className = "delte-button";
+    deleteButton.textContent = "x";
+    deleteButton.onclick = function() { // setting the delete
+        deleteItem(ID, itemUrl.textContent);
+    }
+
+    newItem.appendChild(itemUrl);
+    newItem.appendChild(deleteButton);
+
+    urlList.appendChild(newItem);
+}
+
+function deleteItem(ID, Url) {
+    const index = findIndexOfUrl(Url);
+    if (index != null) {
+        deleteDisplayItem(ID);
+        sites.splice(index, 1);
+
+        saveSitesListToMemory();
+    } else {
+        console.error(`ERROR 1: don't have that URL in the database. URL: ${Url}`);
+        console.log(sites);
+    }
+}
+
+function findIndexOfUrl(Url) {
+    for (let i = 0; i < sites.length; ++i) {
+        if (Url === sites[i]) {
+            return i;
+        }
+    }
+
+    return null
+}
+
+function deleteDisplayItem(ID) { // deleting ab HTML object representing deleted element
+    let listContainer = document.getElementById("url-list");
+    var itemToDelete = document.getElementById('item-' + ID);
+    listContainer.removeChild(itemToDelete);
 }

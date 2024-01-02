3c2571fe413a8aed1fd3681f4ae42aeceeb9fc66
H4pps <150532087+H4pps@users.noreply.github.com>
2024-01-02 05:43:55 +0100

    added timer but it works wierd

--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,47 @@
 chrome.runtime.onMessage.addListener(getMessage);
 function getMessage(message, sender, sendResponse) {
     if (message.text === "Start blocking event") {
+        let durationInSeconds = 15;
+
         const blockingWindow = document.createElement("div");
-        blockingWindow.id = "blocking-window";
+        blockingWindow.id = "blocking-window-blocking-extension";
+
+        const timer = document.createElement("div");
+        timer.id = "blocking-timer-section-blocking-extension";
+        const timerHeader = document.createElement("h1");
+        timerHeader.id = "timer-header-blocking-extension";
+        timerHeader.textContent = "Time left:";
+        const timerCountdown = document.createElement("h1");
+        timerCountdown.id = "timer-countdown-blocking-extension";
+        timerCountdown.textContent = convertSecondsToTimerText(durationInSeconds);
+
+        timer.appendChild(timerHeader); // composing a blocking window HTML object
+        timer.appendChild(timerCountdown);
+        blockingWindow.appendChild(timer);
         document.body.appendChild(blockingWindow);
 
-        console.log("added blocking window");
+        function updateTimer() {
+            timerCountdown.textContent = convertSecondsToTimerText(durationInSeconds);
+
+            durationInSeconds--;
+
+            if (durationInSeconds < 0) {
+                clearInterval(timerInterval);
+                timerCountdown.textContent = 'Timer Expired!';
+                blockingWindow.remove();
+            }
+        }
+
+        // Update the timer every second
+        let timerInterval = setInterval(updateTimer, 1000);
+
+        console.log("event has ended");
     } 
+}
+
+function convertSecondsToTimerText(durationInSeconds) {
+    const minutes = Math.floor(durationInSeconds / 60);
+    const seconds = durationInSeconds % 60;
+
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 }

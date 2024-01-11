7bd711e5e02ef3ff3e055c7cd365580c2cf01b25
H4pps <150532087+H4pps@users.noreply.github.com>
2024-01-11 12:45:17 +0100

    minor changeges to code

--- scripts/content.js
+++ scripts/content.js
@@ -6,7 +6,7 @@ function getMessage(message, sender, sendResponse) {
     // <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     // <link href="https://fonts.googleapis.com/css2?family=Varela&display=swap" rel="stylesheet">`
 
-    if (message.text === "Start blocking event" && message.hostname != previousHostname) {
+    if (message.text === "Start blocking event") {
         previousHostname = message.hostname; // preventing blocking the same hostname several times in a row
 
         let durationInSeconds = 15; // setting time for the countdown timer

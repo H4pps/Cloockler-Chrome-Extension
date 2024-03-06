1567fc16eaec0c0f5149392bcba2265dba090e6b
H4pps <150532087+H4pps@users.noreply.github.com>
2024-03-06 12:59:55 +0100

    added timer countdown + proper styling of the block page

--- blockPage/blockPage.js
+++ blockPage/blockPage.js
@@ -1,39 +1,28 @@
-// let durationInSeconds = 15; // setting time for the countdown timer
-
-//         const blockingWindow = document.createElement("div");
-//         blockingWindow.id = "blocking-window-blocking-extension";
-
-//         const timer = document.createElement("div");
-//         timer.id = "blocking-timer-section-blocking-extension";
-//         const timerHeader = document.createElement("h1");
-//         timerHeader.id = "timer-header-blocking-extension";
-//         timerHeader.textContent = "You still have time to change your mind";
-//         const timerCountdown = document.createElement("h1");
-//         timerCountdown.id = "timer-countdown-blocking-extension";
-//         timerCountdown.textContent = convertSecondsToTimerText(durationInSeconds);
-
-//         timer.appendChild(timerHeader); // composing a blocking window HTML object
-//         timer.appendChild(timerCountdown);
-//         blockingWindow.appendChild(timer);
-//         document.body.appendChild(blockingWindow);
-
-//         function updateTimer() {
-//             --durationInSeconds;
-
-//             if (durationInSeconds < 0) {
-//                 clearInterval(timerInterval);
-//                 blockingWindow.remove();
-//             } else {
-//                 timerCountdown.textContent = convertSecondsToTimerText(durationInSeconds);
-//             }
-//         }
-
-//         // Update the timer every second
-//         let timerInterval = setInterval(updateTimer, 1000);
-
-const blockingText = document.getElementById("blocking-timer");
+const timerCountdown = document.getElementById("timer-countdown");
 
 chrome.runtime.sendMessage({text: "get blocking time"})
 .then(response => {
-    blockingText.textContent += response.time;
+    let durationInSeconds = response.time; // setting time for the countdown time
+    timerCountdown.textContent = convertSecondsToTimerText(durationInSeconds);
+
+    function updateTimer() {
+        --durationInSeconds;
+
+        if (durationInSeconds < 0) {
+            clearInterval(timerInterval);
+            blockingWindow.remove();
+        } else {
+            timerCountdown.textContent = convertSecondsToTimerText(durationInSeconds);
+        }
+    }
+
+    // Update the timer every second
+    let timerInterval = setInterval(updateTimer, 1000);
 });
+
+let convertSecondsToTimerText = durationInSeconds => {
+    const minutes = Math.floor(durationInSeconds / 60);
+    const seconds = durationInSeconds % 60;
+
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}

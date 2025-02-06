f257956f29d74ff12551081beed19b1abb37563a
H4pps <150532087+H4pps@users.noreply.github.com>
2025-02-06 15:02:39 +0100

    add timer saving

--- scripts/blockPage.js
+++ scripts/blockPage.js
@@ -1,26 +1,41 @@
+const TIME_KEY = "savedTime";
 const timerCountdown = document.querySelector("#timer-countdown");
 
-chrome.runtime.sendMessage({ text: "get blocking time" }).then((response) => {
-  let durationInSeconds = response.time; // setting time for the countdown time
-  timerCountdown.textContent = convertSecondsToTimerText(durationInSeconds);
-
-  function updateTimer() {
-    --durationInSeconds;
-
-    if (durationInSeconds < 0) {
-      clearInterval(timerInterval);
-    } else {
-      timerCountdown.textContent = convertSecondsToTimerText(durationInSeconds);
-    }
+const getStartingTime = async () => {
+  const storageData = await chrome.storage.session.get(TIME_KEY);
+  console.log("savedTime in session storage:", storageData[TIME_KEY])
+  if (typeof storageData[TIME_KEY] === "undefined") {
+    const response = await chrome.runtime.sendMessage({ text: "get blocking time" });
+    return response.time;
   }
+  
+  return storageData[TIME_KEY];
+}
 
-  // Update the timer every second
-  let timerInterval = setInterval(updateTimer, 1000);
-});
-
-let convertSecondsToTimerText = (durationInSeconds) => {
+const convertSecondsToTimerText = (durationInSeconds) => {
   const minutes = Math.floor(durationInSeconds / 60);
   const seconds = durationInSeconds % 60;
 
   return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 };
+
+getStartingTime().then((time) => {
+  console.log("time in blockpage:", time)
+  timerCountdown.textContent = convertSecondsToTimerText(time);
+
+  function updateTimer() {
+    --time;
+
+    if (time < 0) {
+      clearInterval(time);
+    } else {
+      chrome.storage.session.set({ [TIME_KEY]: time });
+      timerCountdown.textContent = convertSecondsToTimerText(time);
+    }
+  }
+
+  setTimeout(() => {
+    updateTimer();
+    setInterval(updateTimer, 1000);
+  }, 400);
+});
